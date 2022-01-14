c285948380aa87f72b5aa48779771b25be30d369
torlakla <83416084+torlakla@users.noreply.github.com>
2022-01-14 15:24:08 +0200

    Add files via upload

---
+++ JAVA-OOP/Zoo/Animal.java
@@ -0,0 +1,14 @@
+package Zoo;
+
+public class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+}
