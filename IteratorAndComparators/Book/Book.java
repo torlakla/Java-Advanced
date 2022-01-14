7f20f8ae4a0eef748260d2f79f13277eb463d2ca
torlakla <83416084+torlakla@users.noreply.github.com>
2022-01-14 14:57:58 +0200

    Add files via upload

---
+++ IteratorAndComparators/Book/Book.java
@@ -0,0 +1,52 @@
+package Lab.IteratorsAndComparators.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return this.authors;
+    }
+
+    private void setAuthors(String... authors) {
+        this.authors = new ArrayList<>();
+        for (String author : authors) {
+            this.authors.add(author);
+        }
+    }
+
+//    @Override
+//    public int compareTo(Book other) {
+//        int result = this.title.compareTo(other.title);
+//        if(result==0){
+//            result=Integer.compare(this.year,other.year);
+//        }
+//        return result;
+//    }
+}
